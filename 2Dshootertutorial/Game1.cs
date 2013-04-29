b68566aef32302d4f9eaa556cc244b239184d65a
SunTinWong <azn.c.wong@hotmail.com>
2013-04-29 11:23:46 -0700

    Added player class

---
+++ 2Dshootertutorial/Game1.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+
+namespace _2Dshootertutorial {
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Player p = new Player(); //make player
+
+        public Game1() {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+            //Set some basic stuff for my game
+            graphics.IsFullScreen = false; //set it to full screen no
+            graphics.PreferredBackBufferWidth = 600; //set the screen dimension width
+            graphics.PreferredBackBufferHeight = 700; //set the screen dimension height
+            this.Window.Title = "MySpaceShooter"; //set window title
+        }
+
+        
+        //init funciton
+        protected override void Initialize() {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+       
+        //load content
+        protected override void LoadContent() {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            p.LoadContent(Content); //load the player
+
+        }
+
+
+       
+        //unload
+        protected override void UnloadContent() {
+            // TODO: Unload any non ContentManager content here
+        }
+
+
+       //update
+        protected override void Update(GameTime gameTime) {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
+                this.Exit();
+
+            p.Update(gameTime);
+            base.Update(gameTime);
+        }
+
+       //draw
+        protected override void Draw(GameTime gameTime) {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //Do all the player drawing
+            spriteBatch.Begin();
+            p.Draw(spriteBatch);
+            spriteBatch.End();
+
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ 2Dshootertutorial/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace _2Dshootertutorial {
+    public class Player {
+
+        public Texture2D texture;
+        public Vector2 position;
+        public int speed;
+
+        //Collision varibles
+        public Rectangle boundingBox;
+        public bool isColliding;
+
+        //Defualt Constructor
+        public Player(){
+
+            //set some varibles
+            texture = null;
+            position = new Vector2(300, 600);
+            speed = 10;
+            isColliding = false;
+            
+        }
+
+        //load content
+        public void LoadContent(ContentManager Content){
+            texture = Content.Load<Texture2D>("ship");
+        }
+
+        //draw funciton
+        public void Draw(SpriteBatch spriteBatch){
+            spriteBatch.Draw(texture, position, Color.White);
+
+        }
+
+        //update function
+        public void Update(GameTime gameTime) {
+
+        }
+
+    }
+}
